4af8dcab249e919aa7d699ec1510e342290f7327
Leander Lismond <36822732+l5r@users.noreply.github.com>
2025-02-15 11:13:21 +0100

    Implement home page

--- lib/pintelier_web/controllers/page_controller.ex
+++ lib/pintelier_web/controllers/page_controller.ex
@@ -2,8 +2,8 @@ defmodule PintelierWeb.PageController do
   use PintelierWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    conn
+    |> assign(:consumptions, Pintelier.Drinking.list_global_consumptions())
+    |> render(:home)
   end
 end
